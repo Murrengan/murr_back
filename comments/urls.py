09a728746ae4129d3b6e9fbf9e2e35ee1b304af6
almaz <a.ilaletdinov@yandex.ru>
2020-06-27 23:47:30 +0000

    CRUD functional

--- comments/urls.py
+++ comments/urls.py
@@ -5,12 +5,4 @@ from comments.views import CommentView
 urlpatterns = [
     # CRUD
     path('', CommentView.as_view(), name='CommentView')
-    # Create
-    # path('create/', ...),
-    # Read,
-    # path('', ...),
-    # Update
-    # path('', ...),
-    # Delete
-    # path('', ...)
 ]
--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -14,9 +14,31 @@ class CommentView(APIView):
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = CommentSerializer(request.data)
+        serializer = CommentSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+
+    def put(self, request):
+        pk = request.query_params['comment_id']
+        comment = get_object_or_404(Comment.objects.all(), pk=pk)
+        data = request.data
+        serializer = CommentSerializer(instance=comment, data=data, partial=True)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=204)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request):
+        pk = request.query_params['comment_id']
+        comment = get_object_or_404(Comment.objects.all(), pk=pk)
+        comment.delete()
+        return Response(
+            {
+                'success': True,
+                'message': f'Comment with id {pk} has been deleted'
+            }, status=204
+        )
