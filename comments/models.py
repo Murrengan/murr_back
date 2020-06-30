b1edd08490b9b15f249973df1c224e20780bfc0f
almaz <a.ilaletdinov@yandex.ru>
2020-06-30 22:20:55 +0000

    Update Comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from murr_card.models import MurrCard
@@ -10,6 +11,13 @@ class Comment(models.Model):
     owner = models.ForeignKey(Murren, on_delete=models.CASCADE, related_name='comments',
                               verbose_name='Автор комментария')
     text = models.TextField(max_length=512)
+    parent = models.ForeignKey('self', on_delete=models.PROTECT, verbose_name='Родительский комментарий', null=True)
+    depth = models.IntegerField(verbose_name='Глубина комментария', default=1,
+                                validators=[MaxValueValidator(5), MinValueValidator(1)])
 
     def __str__(self):
         return f'{self.owner}: "{self.text}"'
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
