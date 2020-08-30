e68bbaa32a38023e529ec83ba21d9986914ab61c
Murrengan <egor.a.komarov@yandex.ru>
2020-08-30 15:06:57 +0300

    update for oauth

--- murren/urls.py
+++ murren/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('all/', GetAllMurrens.as_view(), name='get_all_murrens'),
     path('<int:pk>/', PublicMurrenInfo.as_view(), name='get_murren_info_by_pk'),
 
-    url('token_create/', obtain_jwt_token),
-    path('oauth/google/', GoogleLogin.as_view(), name='google_login')
+    url('token_create/', obtain_jwt_token, name='obtain_jwt_token'),
+    path('oauth/google/', GoogleLogin.as_view(), name='socialaccount_signup')
 
 ]
