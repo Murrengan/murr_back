971566c4bf9ede89dd386ad8c7c3b24377abf03d
N1LEX <->
2020-09-01 21:55:28 +0500

    Revert "da"
    This reverts commit 94ff1ff9455c92503a2ee625f3bfadf171d1a879.

--- murr_card/urls.py
+++ murr_card/urls.py
@@ -6,7 +6,7 @@ from rest_framework.routers import DefaultRouter
 from .views import MurrCardViewSet, EditorImageForMurrCardView
 
 router = DefaultRouter()
-router.register('', MurrCardViewSet, basename='murr_card')
+router.register('', MurrCardViewSet)
 
 urlpatterns = [
     path('save_editor_image/', csrf_exempt(EditorImageForMurrCardView.as_view()), name='save_editor_image'),
