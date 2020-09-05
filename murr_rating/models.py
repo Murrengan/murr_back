aa4eefccdc98562260b693398d26121fd70a9224
N1LEX <->
2020-09-05 11:31:05 +0500

    [murr_rating, models, rating]: changed max_length attr from 4 to 7 in rating_type field

--- murr_rating/models.py
+++ murr_rating/models.py
@@ -5,7 +5,7 @@ from murren.models import Murren
 
 class Rating(models.Model):
     murren = models.ForeignKey(Murren, verbose_name='Муррен', related_name='ratings', on_delete=models.CASCADE)
-    rating_type = models.CharField('Тип оценки', max_length=4, null=True, blank=True)
+    rating_type = models.CharField('Тип оценки', max_length=7, null=True, blank=True)
     object_id = models.PositiveIntegerField('Идентификатор объекта')
     object_type = models.CharField('Тип объекта', max_length=255)
 
