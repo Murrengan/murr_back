b5684610ba54c30564651d9cb0994d31e4398852
Alexandr Sukhrin <alexandrvirtual@gmail.com>
2020-09-12 15:10:49 +0300

    add search and tests

--- murr_search/tests.py
+++ murr_search/tests.py
@@ -1,3 +1,59 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from murr_card.models import MurrCard
+from murren.models import Murren
+
+
+class SearchMurrenViewTests(APITestCase):
+
+    def setUp(self) -> None:
+        self.api = r'/api/search/murren/?search='
+        self.test_user = Murren.objects.create_user(
+            username='murren',
+            email='admin@murrengan.ru',
+        )
+
+    def tearDown(self) -> None:
+        del self.api
+        del self.test_user
+
+    def test_search_murren_username(self) -> None:
+        search_text = 'murr'
+        self.client.force_authenticate(self.test_user)
+        response = self.client.get(f'{self.api}{search_text}')
+
+        self.assertEqual(response.status_code, 200)
+
+        murren = response.data['results'][0]
+        self.assertEqual(murren['username'], 'murren')
+        self.assertEqual(murren['email'], 'admin@murrengan.ru')
+
+
+class SearchMurrCardViewTests(APITestCase):
+
+    def setUp(self) -> None:
+        self.api = r'/api/search/murr_card/?search='
+        self.test_user = Murren.objects.create(
+            username='murren',
+            email='admin@murrengan.ru',
+        )
+        self.test_card = MurrCard.objects.create(
+            title='murr card',
+            content='murren test card',
+            owner=self.test_user,
+        )
+
+    def tearDown(self) -> None:
+        del self.api
+        del self.test_user
+
+    def test_search_murren_name(self) -> None:
+        search_text = 'card'
+        self.client.force_authenticate(self.test_user)
+        response = self.client.get(f'{self.api}{search_text}')
+
+        murr_card = response.data['results'][0]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(murr_card['title'], 'murr card')
+        self.assertEqual(murr_card['content'], 'murren test card')
+        self.assertEqual(murr_card['owner_name'], 'murren')
--- murr_search/urls.py
+++ murr_search/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from murr_search.views import SearchMurrenView, SearchMurrCardView
 
 urlpatterns = [
-    path('', SearchResultsView.as_view(), name='search'),
+    path('murren/', SearchMurrenView.as_view(), name='search_murren'),
+    path('murr_card/', SearchMurrCardView.as_view(), name='search_murr_card'),
+
 ]
