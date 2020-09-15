15b12215ecda95622dfb4730814b575d68929530
Your Name <you@example.com>
2020-09-15 10:06:33 +0300

    added blank=true to like/dislike logic

--- murr_rating/models.py
+++ murr_rating/models.py
@@ -4,8 +4,8 @@ from murren.models import Murren
 
 
 class RatingAbstractModel(models.Model):
-    liked_murrens = models.ManyToManyField(Murren, related_name='liked_%(class)ss')
-    disliked_murrens = models.ManyToManyField(Murren, related_name='disliked_%(class)ss')
+    liked_murrens = models.ManyToManyField(Murren, related_name='liked_%(class)ss', blank=True)
+    disliked_murrens = models.ManyToManyField(Murren, related_name='disliked_%(class)ss', blank=True)
     rating = models.IntegerField('Рейтинг', blank=True, default=0)
 
     class Meta:
